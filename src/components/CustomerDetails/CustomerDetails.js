ce61f4bd70aa91d0505dc696b9bfe3085f24532d
bheadwhite <bdub@bheadwhites-MacBook-Pro.local>
2019-07-06 00:00:39 -0600

    added some divs to customer details

--- src/components/CustomerDetails/CustomerDetails.js
+++ src/components/CustomerDetails/CustomerDetails.js
@@ -3,13 +3,23 @@ import "./CustomerDetails.css"
 
 const Details = () => (
 	<div className='customerDetails'>
+		<div>
 		<label htmlFor='name'>Name</label>
 		<input type='text' id='name' required onChange={e => this.setState({ cstName: e.target.value })} />
+
+		</div>
+		<div>
+
 		<label>Phone Number:</label>
 		<input type='tel' placeholder='(xxx)-xxx-xxxx' required onChange={e => this.setState({ cstNumber: e.target.value })} />
+		<div>
 		<label>Is a text ok?</label>
 		<input type='checkbox' />
+		</div>
+		</div>
+		<div>
 		<label>additional contact details:</label>
+		</div>
 		<textarea name='' id='' cols='30' rows='10' />
 		<button onClick={() => this.sendEmail()}>Submit Order</button>
 	</div>
