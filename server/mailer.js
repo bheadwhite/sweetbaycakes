5003422df457a051e16d8fdf7be7ce175b1643df
Brent Whitehead <bheadwhite@gmail.com>
2019-07-07 21:59:15 -0600

    mailer up for laurel

--- server/mailer.js
+++ server/mailer.js
@@ -1,59 +1,57 @@
 const nodemailer = require("nodemailer"),
 	{ google } = require("googleapis"),
-  OAuth2 = google.auth.OAuth2
-  require('dotenv').config()
-  
-const oauth2Client = new OAuth2(
-  process.env.clientId,
-  process.env.clientSecret,
-  process.env.redirect
-)
+	OAuth2 = google.auth.OAuth2
+require("dotenv").config()
+
+const oauth2Client = new OAuth2(process.env.clientId, process.env.clientSecret, process.env.redirect)
 
 oauth2Client.setCredentials({
-  refresh_token: process.env.refreshToken
+	refresh_token: process.env.refreshToken
 })
-const sendMail = async (outgoing) => {
-  const tokens = await oauth2Client.refreshAccessToken()
-  const accessToken = tokens.credentials.access_token
+const sendMail = async outgoing => {
+	const tokens = await oauth2Client.refreshAccessToken()
+	const accessToken = tokens.credentials.access_token
 
-  const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-          type: "OAuth2",
-          user: process.env.nodeEmail,
-          clientId: process.env.clientId,
-          clientSecret: process.env.refreshToken,
-          accessToken: accessToken
-
-      }
-  });
-  let makeMe = ''
-  for(key in outgoing){
-    if(key.startsWith('make') && key !== "makeText"){
-      makeMe = makeMe.concat(`<li>${key.slice(4)}</li>`)
-    }
+	const transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			type: "OAuth2",
+			user: process.env.nodeEmail,
+			clientId: process.env.clientId,
+			clientSecret: process.env.refreshToken,
+			accessToken: accessToken
+		}
+	})
+	let makeMe = ""
+	for (key in outgoing) {
+		if (key.startsWith("make") && key !== "makeText") {
+			makeMe = makeMe.concat(`<li>${key.slice(4)}</li>`)
+		}
   }
-  if(outgoing.makeText){
-    outgoing.makeText = 'yes'
-  } else {
-    outgoing.makeText = 'no'
+  if (!outgoing.cakeFillings){
+    outgoing.cakeFillings = 'none'
   }
-  let attachments= []
-  if(outgoing.files){
-    outgoing.files.forEach(file => {
-      attachments.push({cid: file})
-    })
-  }
-  console.log(attachments)
-  let info = await transporter.sendMail({
-      from: '"bwhitehe@gmail.com" <bwhitehe@gmail.com>', // sender address
-      to: "bheadwhite@gmail.com", // list of receivers
-      subject: `sweetbaycakes Order for ${outgoing.name}`, // Subject line
-      generateTextFromHTML: true, // plain text body
-      attachments: attachments,
-      html: `
-      <h3 style={{color: "green"}}>Order for ${outgoing.name}</h3>
-      <h5>phone number: ${outgoing.phoneNumber}</h5>
+	if (outgoing.makeText) {
+		outgoing.makeText = "yes"
+	} else {
+		outgoing.makeText = "no"
+	}
+	let attachments = []
+	if (outgoing.files) {
+		outgoing.files.forEach(file => {
+			attachments.push({ cid: file })
+		})
+	}
+	let info = await transporter.sendMail({
+		from: '"sweetbaycakes29@gmail.com" <sweetbaycakes29@gmail.com>', // sender address
+		to: "lfletcher29@gmail.com", // list of receivers
+		subject: `Cake Order for ${outgoing.name}`, // Subject line
+		generateTextFromHTML: true, // plain text body
+    html: `
+      <h3><span style="color:green">Order for </span><span style="font-size:3rem">${outgoing.name}</span>
+      <br/>
+      <span style="color:blue">Contact:</span><span style="color:orange"> ${outgoing.phoneNumber}</span>
+      </h3>
       <hr/>
       <p>Order:</p>
       <ul>
@@ -63,6 +61,7 @@ const sendMail = async (outgoing) => {
       <p>orientation: ${outgoing.cakeOrientation}</p>
       <p>flavor: ${outgoing.cakeFlavor}</p>
       <p>Icing/Cream: ${outgoing.cakeCream}</p>
+      <p>Extra Cake Fillings: ${outgoing.cakeFillings}</p>
       <p>Additional Requests: ${outgoing.cakeRequest}</p>
       <hr/>
 
@@ -72,12 +71,11 @@ const sendMail = async (outgoing) => {
       <p>${outgoing.contactDetails}</p>
       </div>
       ` // html body
-    });
+	})
 
-  console.log("message sent: %s", info.messageId);
+	console.log("message sent: %s", info.messageId)
 
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
-
+	console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
 }
 
 module.exports = sendMail
