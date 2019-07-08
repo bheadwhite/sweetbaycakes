c6fe1accde07ce296a512c425a6462e2f07f74dd
Brent Whitehead <bheadwhite@gmail.com>
2019-07-07 20:15:37 -0600

    prepping for heroku deployment

--- server/index.js
+++ server/index.js
@@ -6,10 +6,9 @@ const express = require("express"),
 	session = require("express-session"),
 	helmet = require("helmet"),
 	mailer= require("./mailer.js"),
-	serverless = require('serverless-http')
 require("dotenv").config()
 
-app.use(bodyParser.json(), helmet(), express.static(`${__dirname}/../build`))
+app.use(cors(), bodyParser.json(), helmet(), express.static(`${__dirname}/../build`))
 
 app.post("/api/submitForm", (req, res) => {
 	console.log(req.body)
@@ -24,5 +23,7 @@ app.use("/*", (req, res) => {
 		root: `${__dirname}/..build`
 	})
 })
-module.exports = app
-module.exports.handler = serverless(app)
+const port = process.env.port || 3001
+app.listen(port, ()=>{
+	console.log(`listening on ${port}`)
+})
