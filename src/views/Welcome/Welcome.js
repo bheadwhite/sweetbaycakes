78ca6964b10ddd5843c34fbcd3d9fb23d67421fc
Brent Whitehead <bheadwhite@gmail.com>
2019-09-06 22:16:30 -0600

    added styles of cakes sections

--- src/views/Welcome/Welcome.js
+++ src/views/Welcome/Welcome.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-router-dom"
+import './Welcome.css'
 
-export default function Welcome(){
-  return (
-    
-    <div>Welcome to Sweet Bay Cakes</div>
-
-  )
+export default function Welcome() {
+	return (
+		<>
+			<div className="welcome">Welcome to Sweet Bay Cakes</div>
+			<Link to='/order'>Order</Link>
+      <div className='wrapper'>
+        <div className="box"><p>Wedding Cakes</p></div>
+        <div className="box"><p>Birthday Cakes</p></div>
+        <div className="box"><p>Special Occasion Cakes</p></div>
+        <div className="box"><p>Cupcakes</p></div>
+        <div className="box"><p>Other Treats</p></div>
+      </div>
+		</>
+	)
 }
